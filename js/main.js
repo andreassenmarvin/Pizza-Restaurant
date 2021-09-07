f180d499c19135c5dad1727c7ceebd11b79c5452
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-07 23:34:24 +0300

    Add page about

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+// Slideshow images
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Slideshow controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("carousel-item");
+    var indicators = document.getElementsByClassName("indicators");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < indicators.length; i++) {
+        indicators[i].className = indicators[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    indicators[slideIndex - 1].className += " active";
+}
+
+
 // Navbar transition on window scroll
 $(function () {
     var navbar = document.getElementById("navbar");
